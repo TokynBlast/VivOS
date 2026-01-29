0ad85d87e1d9ac52da2652bc3a7d6ee57bf160db
🍙 𝑹𝒖𝒓𝒊 🎋 <tokynblast@gmail.com>
2026-01-29 10:07:51 -0800

    Add boot info

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,19 +3,32 @@
 
 use uefi::prelude::*;
 use uefi::{print, println};
+use uefi::proto::media::fs::SimpleFileSystem;
+use uefi::proto::media::file::{File, FileAttribute, FileMode};
 
 #[entry]
 fn main() -> Status {
-  uefi::helpers::init().unwrap();
+  uefi::helpers::init(&mut system_table).unwrap();
+  //uefi::helpers::init().unwrap();
 
   println!("Booting VivOS"); // :3
 
   struct BootInfo {
-    secure_boot: false
+    secure_boot: bool,
+    // buffer_loc: u64=
+
   }
+
+  let bt = &uefi::helpers::boot_services().unwrap();
+
+  let info = BootInfo {
+    secure_boot: false
+    // bufffer_loc: 2097154 // book 2
+  };
 
   loop {}
 }
+
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
